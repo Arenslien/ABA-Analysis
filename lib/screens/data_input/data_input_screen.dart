0d725914ea9fa1ecbee5fc55af60d36c59905b72
sunoo0210 <aswoo55555@gmail.com>
2021-08-02 17:34:38 +0900

    검색화면 상단바

--- lib/screens/data_input/data_input_screen.dart
+++ lib/screens/data_input/data_input_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class InputScreen extends StatefulWidget {
@@ -8,27 +9,85 @@ class InputScreen extends StatefulWidget {
 }
 
 class _InputScreenState extends State<InputScreen> {
-  final myController = TextEditingController();
+  TextEditingController searchTextEditingController = TextEditingController();
+
+  late Future<QuerySnapshot> futureSearchResults;
+
+  controlSearching(str) {
+    print(str);
+    Future<QuerySnapshot> allUsers =
+        useReference.where('profileName', isGreaterThanOrEqualTo: str).get();
+    setState(() {
+      futureSearchResults = allUsers;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('아동 관리'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: '아동 검색',
-              ),
-              controller: myController,
-            ),
-          ],
+      appBar: searchPageHeader(),
+      body: futureSrearchResults == null
+          ? displayNoSearchResultScreen()
+          : displayUsersFoundScreen(),
+    );
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('아동 관리'),
+    //   ),
+    //   body: Padding(
+    //     padding: const EdgeInsets.all(8.0),
+    //     child: Column(
+    //       children: [
+    //         TextField(
+    //           decoration: InputDecoration(
+    //             border: OutlineInputBorder(),
+    //             labelText: '아동 검색',
+    //           ),
+    //           onChanged: (String str) {
+    //             setState(() => a = str);
+    //           },
+    //         ),
+    //         SizedBox(
+    //           height: 10,
+    //         ),
+    //         Text(a),
+    //         // ListView(
+    //         //   children: [
+    //         //     Container(
+    //         //       child: Text(a),
+    //         //     ),
+    //         //   ],
+    //         // ),
+    //       ],
+    //     ),
+    //   ),
+    // );
+  }
+
+  AppBar searchPageHeader() {
+    return AppBar(
+      title: TextFormField(
+        controller: searchTextEditingController,
+        decoration: InputDecoration(
+          hintText: 'Search here...',
+          hintStyle: TextStyle(color: Colors.grey),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          filled: true,
+          prefixIcon: Icon(Icons.person_pin, color: Colors.white, size: 30),
+          suffixIcon: IconButton(
+            icon: Icon(Icons.clear, color: Colors.white),
+            onPressed: () {
+              searchTextEditingController.clear();
+            },
+          ),
         ),
+        style: TextStyle(fontSize: 18, color: Colors.white),
+        onFieldSubmitted: controlSearching,
       ),
     );
   }
