37b71d9e15f343437a78e81f36d131d71361deb4
sunoo0210 <aswoo55555@gmail.com>
2021-08-05 15:18:32 +0900

    아동 추가

---
+++ lib/screens/child_management/child_input_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class ChildInputScreen extends StatefulWidget {
+  const ChildInputScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChildInputScreenState createState() => _ChildInputScreenState();
+}
+
+class _ChildInputScreenState extends State<ChildInputScreen> {
+  Widget buildTextField(
+      {required String name, TextEditingController? controller}) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          //border: OutlineInputBorder(),
+          labelText: name,
+          hintText: name,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Child'),
+      ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              buildTextField(name: 'Name'),
+              buildTextField(name: 'Age'),
+              buildTextField(name: 'Gender'),
+              // Padding(
+              //   padding: const EdgeInsets.all(16.0),
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //     children: [
+              //       Expanded(
+              //         child: DropdownButtonFormField(
+              //           decoration: InputDecoration(
+              //             border: OutlineInputBorder(),
+              //             labelText: 'Item',
+              //           ),
+              //           items: [],
+              //         ),
+              //       ),
+              //       IconButton(
+              //         icon: Icon(Icons.add_circle_outline_rounded),
+              //         onPressed: () {},
+              //       ),
+              //     ],
+              //   ),
+              // ),
+              RaisedButton(
+                child: Text('Save'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/child_management/child_screen.dart
+++ lib/screens/child_management/child_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:aba_analysis/screens/child_management/search_bar.dart';
+import 'package:aba_analysis/screens/child_management/child_input_screen.dart';
 
 class ChildScreen extends StatefulWidget {
   const ChildScreen({Key? key}) : super(key: key);
@@ -11,28 +12,42 @@ class ChildScreen extends StatefulWidget {
 class _ChildScreenState extends State<ChildScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: SearchBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              icon: Icon(Icons.add_circle_outline_rounded),
-              iconSize: 150,
-              color: Colors.black,
-              onPressed: () {},
-            ),
-            Text(
-              'Add Child',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w500,
-                fontSize: 40,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: SearchBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.group,
+                color: Colors.grey,
+                size: 150,
               ),
-              
-            ),
-          ],
+              Text(
+                'Add Child',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 40,
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.add_rounded,
+            size: 40,
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ChildInputScreen()),
+            );
+          },
+          backgroundColor: Colors.black,
         ),
       ),
     );
