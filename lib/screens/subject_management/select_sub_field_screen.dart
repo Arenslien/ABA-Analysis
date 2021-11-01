d7a75b1682cb97f6133ab03c2ba041d8371dd0b4
LeeJinGue <hippo9851@gmail.com>
2021-11-02 03:58:15 +0900

    하위영역 생성시 하위영역, 하위목록 중복체크 구현

--- lib/screens/subject_management/select_sub_field_screen.dart
+++ lib/screens/subject_management/select_sub_field_screen.dart
@@ -1,6 +1,8 @@
 import 'package:aba_analysis/models/program_field.dart';
 import 'package:aba_analysis/models/sub_field.dart';
+import 'package:aba_analysis/models/test_item.dart';
 import 'package:aba_analysis/provider/program_field_notifier.dart';
+import 'package:aba_analysis/provider/test_item_notifier.dart';
 import 'package:aba_analysis/screens/subject_management/sub_field_view_screen.dart';
 import 'package:aba_analysis/screens/subject_management/sub_field_input_screen.dart';
 import 'package:aba_analysis/services/firestore.dart';
@@ -38,7 +40,7 @@ class _SelectSubfieldScreenState extends State<SelectSubfieldScreen> {
                     SubFieldInputScreen(program: widget.program)),
           );
         },
-        backgroundColor: Colors.black,
+        backgroundColor: mainGreenColor,
       ),
       appBar: AppBar(
         title: Text(
@@ -48,17 +50,25 @@ class _SelectSubfieldScreenState extends State<SelectSubfieldScreen> {
         backgroundColor: mainGreenColor,
       ),
       body: ListView.builder(
-        itemCount: context.watch<ProgramFieldNotifier>().readSubFieldList(widget.program.title).length,
+        itemCount: context
+            .watch<ProgramFieldNotifier>()
+            .readSubFieldList(widget.program.title)
+            .length,
         itemBuilder: (BuildContext context, int index) {
           return buildListTile(
-              titleText: context.watch<ProgramFieldNotifier>().readSubFieldList(widget.program.title)[index].subFieldName,
+              titleText: context
+                  .watch<ProgramFieldNotifier>()
+                  .readSubFieldList(widget.program.title)[index]
+                  .subFieldName,
               titleSize: 20,
               onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) =>
-                        SelectSubitemScreen(subField: context.watch<ProgramFieldNotifier>().readSubFieldList(widget.program.title)[index]),
+                    builder: (context) => SelectSubitemScreen(
+                        subField: context
+                            .watch<ProgramFieldNotifier>()
+                            .readSubFieldList(widget.program.title)[index]),
                   ),
                 );
               },
@@ -68,7 +78,7 @@ class _SelectSubfieldScreenState extends State<SelectSubfieldScreen> {
                 child: IconButton(
                     onPressed: () {
                       // DB에서 SubField 가져와서 삭제
-              
+
                       // 진짜 삭제할건지 물어보는 다이얼로그
                       showDialog(
                           context: context,
@@ -103,14 +113,43 @@ class _SelectSubfieldScreenState extends State<SelectSubfieldScreen> {
                                       backgroundColor: Colors.red,
                                     ),
                                     onPressed: () async {
+                                      String subFieldNameForDel = context
+                                          .read<ProgramFieldNotifier>()
+                                          .readSubFieldList(
+                                              widget.program.title)[index]
+                                          .subFieldName;
+
                                       // DB에서 서브필드를 삭제한다.
-                                      await store.deleteSubField(convertProgramFieldTitle(widget.program.title)!, index);
-              
+                                      await store.deleteSubField(
+                                          convertProgramFieldTitle(
+                                              widget.program.title)!,
+                                          index);
+
                                       // 해당 서브필드를 삭제한다.
-                                      context.read<ProgramFieldNotifier>().updateProgramFieldList(await store.readProgramField());
-              
+                                      context
+                                          .read<ProgramFieldNotifier>()
+                                          .updateProgramFieldList(
+                                              await store.readProgramField());
+
                                       // setState로 삭제한거 업데이트.
-              
+                                      // DB에서 삭제한 서브필드의 테스트 아이템 삭제
+
+                                      List<TestItem> testItemList = context
+                                          .read<TestItemNotifier>()
+                                          .testItemList;
+                                      for (TestItem testItem in testItemList) {
+                                        if (testItem.subField ==
+                                            subFieldNameForDel) {
+                                          await store.deleteTestItem(
+                                              testItem.testItemId);
+                                          context
+                                              .read<TestItemNotifier>()
+                                              .updateTestItemList(await store
+                                                  .readAllTestItem());
+                                          //.removeTestItem(testItem);
+                                        }
+                                      }
+
                                       Navigator.pop(context);
                                     }),
                               ],
